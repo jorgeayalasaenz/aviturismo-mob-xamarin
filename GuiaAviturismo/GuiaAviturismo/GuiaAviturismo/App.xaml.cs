fd0fe61e4c8fd73bab1ba49a31f7db2c2584cea4
Ana Ma. Jalca <amjalca@espol.edu.ec>
2016-12-26 19:05:59 -0500

    Inicio de proyecto, aviturismo
    Creación de MasterPage, MainPage, MenuPage y configuración del App.xaml

---
+++ GuiaAviturismo/GuiaAviturismo/GuiaAviturismo/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using GuiaAviturismo.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GuiaAviturismo
+{
+    public partial class App : Application
+    {
+        public static NavigationPage Navigator { get; internal set; }
+
+        public App()
+        {
+            InitializeComponent(); //se inicializar el diccionario de recursos
+
+            MainPage = new MasterPage();
+            // The root page of your application
+            //var content = new ContentPage
+            //{
+            //    Title = "GuiaAviturismo",
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
+
+            //MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ GuiaAviturismo/GuiaAviturismo/GuiaAviturismo/Pages/MasterPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GuiaAviturismo.Pages
+{
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            App.Navigator = this.Navigator;
+        }
+    }
+}
